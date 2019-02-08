343a5d5ccf7c5ae2e010107b766ae0126ee7ec7a
Raj-1997 <33152265+Raj-1997@users.noreply.github.com>
2019-02-08 00:22:13 +0000

    Android Studio Base files
    Just added the Android Studio base files
    
    Co-Authored-By: raj-1997 <raj-1997@users.noreply.github.com>

---
+++ app/src/main/java/brunel/group28/cookking/MainActivity.java
@@ -0,0 +1,13 @@
+package brunel.group28.cookking;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
