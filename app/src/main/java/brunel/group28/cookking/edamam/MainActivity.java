b3432b5d2400a45372b62c1d52f5cbf3c8e0012c
Raj <1707199@brunel.ac.uk>
2019-03-26 13:04:33 +0000

    Recipes Regex
    The recipes regex works for one of the recipes, which is good enough for testing purposes. I shall regex other, and more recipes later, but for now this is enough to attempt to move further into development of the algorithm for the program.

--- app/src/main/java/brunel/group28/cookking/edamam/MainActivity.java
+++ app/src/main/java/brunel/group28/cookking/edamam/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
 
     static String RecipeName; // recipe name
     static ArrayList<String> RecipeIngredients; // RecipeIngredients with getting rid of unuseful words or phrases.
+    static ArrayList<Double> IngredientsWeights; //
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,27 +45,46 @@ public class MainActivity extends AppCompatActivity {
                 //System.out.println(response.body().getHits().get(0).getRecipe().size());
 
 
+                //START OF GETTING RECIPE NAME
+
                 System.out.println(response.body().getHits().get(0).getRecipe().getLabel());
                 String RecipeName = response.body().getHits().get(0).getRecipe().getLabel();
-                getRecipeName(RecipeName);
+                getRecipeName(RecipeName); // get and set the recipe name
+
+                //END OF GETTING RECIPE NAME
+
+
+                //START OF GETTING RECIPE INGREDIENTS
 
                 System.out.println(response.body().getHits().get(0).getRecipe().getIngredientLines());
                 ArrayList<String> Ingredients = new ArrayList<String>();
 
 
+
                 for(int i = 0; i < response.body().getHits().get(0).getRecipe().getIngredients().size(); i++){
                     Ingredients.add(response.body().getHits().get(0).getRecipe().getIngredients().get(i).getText());
+                   // System.out.println(i);
                     System.out.println(response.body().getHits().get(0).getRecipe().getIngredients().get(i).getText());
                 }
 
-                RegexRecipes(Ingredients);
+                RegexRecipes(Ingredients); //regular expression the recipes
+
+                // END OF GETTING RECIPE INGREDIENTS
+
+
+                // Start of setting recipes
+                ArrayList<Double> IngredientsWeights = new ArrayList<>();
+
+                for(int i = 0; i < response.body().getHits().get(0).getRecipe().getIngredients().size(); i++){
+                    IngredientsWeights.add(response.body().getHits().get(0).getRecipe().getIngredients().get(i).getWeight());
+                }
 
 
 
-               // System.out.println(response.body().getHits());
-                // while(recipe.hasNext)
 
-                //System.out.println("this is the recipe "+recipe.toString());
+
+
+
             }
 
 
@@ -86,13 +106,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public static void RegexRecipes(ArrayList<String> RecipeIngredientsRegex ){
+    public static void RegexRecipes(ArrayList<String> RecipeIngredientsToRegex ){
 
-        ArrayList<String> RegexList = new ArrayList<String>();
+        for(int i = 0; i < RecipeIngredientsToRegex.size(); i++){
+            String ok = RecipeIngredientsToRegex.get(i);
+          //  System.out.println(ok);
+        }
 
-       // .replaceAll(  "0-9,"");
-        // replaceAll("\\p{P}","");
-        // above removes punctuation marks that include all brackets, braces and sq. brackets
+
+
+
+        ArrayList<String> RegexList = new ArrayList<>();
+
+
+
+        ArrayList<String> RecipeIngredientRegexed = new ArrayList<String>();
 
         RegexList.add("peeled");
         RegexList.add("and");
@@ -104,25 +132,46 @@ public class MainActivity extends AppCompatActivity {
         RegexList.add("thawed");
         RegexList.add("pieces");
 
-        for(int i = 0; i < RecipeIngredientsRegex.size(); i++) {
-            RecipeIngredientsRegex.get(i).replaceAll("0-9", "");
-            RecipeIngredientsRegex.get(i).replaceAll("\\p{P}", "");
 
+        System.out.println("STARTING FROM HERE");
+        for(int i = 0; i < RecipeIngredientsToRegex.size(); i++){
+             String okay = RecipeIngredientsToRegex.get(i).replaceAll("peeled","");
+             okay = okay.replaceAll(",","");
+             okay = okay.replaceAll("and","");
+             okay = okay.replaceAll("cut","");
+             okay = okay.replaceAll("into","");
+             okay = okay.replaceAll("chunks","");
+             okay = okay.replaceAll("chopped","");
+             okay = okay.replaceAll("cup","");
+             okay = okay.replaceAll("thawed","");
+             okay = okay.replaceAll("/","");
+             okay = okay.replaceAll("[0-9]","");
+             okay = okay.replaceAll("tablespoons","");
+             okay = okay.replaceAll("tablespoon","");
+             okay = okay.replaceAll("large","");
+             okay = okay.replaceAll("pound","");
+             okay = okay.replaceAll("frozen","");
+             okay = okay.replaceAll("-","");
+             okay = okay.replaceAll("\\p{P}","");
 
+            //okay = okay.substring(0, okay.indexOf("-"));
+            okay = okay.split(" or ")[0];
+
+            okay = okay.trim().replaceAll(" +", " ");
+
+            RecipeIngredientRegexed.add(okay);
         }
-            for (int j = 0; j < RecipeIngredientsRegex.size(); j++) {
-                for(int k = 0; k < RegexList.size(); k++) {
-                    RecipeIngredientsRegex.get(j).replaceAll(RegexList.get(k),"");
-                }
-            }
 
-            System.out.println("testing from here");
 
-            for(int i = 0; i < RecipeIngredientsRegex.size(); i++){
-                System.out.println(RecipeIngredientsRegex.get(i));
-            }
+        System.out.println("okay");
 
-       RecipeIngredients = RecipeIngredientsRegex;
+        for(int i = 0; i < RecipeIngredientRegexed.size(); i++){
+            System.out.println(RecipeIngredientRegexed.get(i));
+        }
+
+
+
+       RecipeIngredients = RecipeIngredientRegexed;
 
     }
 
